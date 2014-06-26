b48b931748588139a531f6ff671adcfffcc565c7
John Koelndorfer <jkoelndorfer@gmail.com>
2014-06-25 20:55:02 -0500

    profile/localenv.sh: Add functionality to execute program.

--- profile/localenv.sh
+++ profile/localenv.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-# This script configures environment variables for local use.
+# This script configures environment variables for local use.  Additionally,
+# $1 will be executed with the given arguments if $1 is specified.
+# The invoked program will be run in the configured environment.
 #
 # We can change these variables to switch to a development environment.
 
@@ -12,3 +14,7 @@ LOCAL_PYTHONPATH="${LOCAL_TOOLS}/lib/python"
 PYTHONPATH="${LOCAL_PYTHONPATH}${PYTHONPATH:+:$PYTHONPATH}"
 
 export LOCAL_DIR LOCAL_TOOLS LOCAL_ETC LOCAL_PYTHONPATH PYTHONPATH
+
+if [ -n "$1" ]; then
+    "$@"
+fi
