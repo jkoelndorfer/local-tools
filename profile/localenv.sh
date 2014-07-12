60115e0fcbf2514955fbf81f902f19f0c25666b2
John Koelndorfer <jkoelndorfer@gmail.com>
2014-07-12 12:01:17 -0500

    profile/localenv.sh: Use more sophisticated pathmunge for PYTHONPATH.
    Multiple instances of LOCAL_PYTHONPATH were showing up in PYTHONPATH if
    multiple login shells were invoked (e.g. in the case of tmux windows).

--- profile/localenv.sh
+++ profile/localenv.sh
@@ -6,12 +6,23 @@
 #
 # We can change these variables to switch to a development environment.
 
+function pathmunge {
+    newcomponent="$1"
+    pathvar="$2"
+
+    if ! echo "$pathvar" | grep -q "$newcomponent"; then
+        echo "$pathvar:$newcomponent"
+    else
+        echo "$pathvar"
+    fi
+}
+
 LOCAL_DIR='/opt/local'
 LOCAL_TOOLS="${LOCAL_DIR}/tools"
 LOCAL_ETC="${LOCAL_DIR}/etc"
 LOCAL_PYTHONPATH="${LOCAL_TOOLS}/lib/python"
 
-PYTHONPATH="${LOCAL_PYTHONPATH}${PYTHONPATH:+:$PYTHONPATH}"
+PYTHONPATH="$(pathmunge "$LOCAL_PYTHONPATH" "$PYTHONPATH")"
 
 export LOCAL_DIR LOCAL_TOOLS LOCAL_ETC LOCAL_PYTHONPATH PYTHONPATH
 
